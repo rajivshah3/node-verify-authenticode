40a6391b5874681c019bb8b1d341f35b0640a72f
Rajiv Shah <rajivshah1@icloud.com>
2021-04-15 13:49:14 -0400

    Throw error on other platforms

--- lib/binding.ts
+++ lib/binding.ts
@@ -1,6 +1,8 @@
-const addon = require('../build/Release/verify-authenticode-native');
-
 const verify = (path: string, expectedSubjectName: string, expectedIssuerName: string, expectedSerialNumber: string): Promise<boolean> => {
+    if (process.platform !== "win32") {
+        throw new Error("verify-authenticode is not compatible with this platform");
+    }
+    const addon = require('../build/Release/verify-authenticode-native');
     return Promise.resolve(addon.verify(path, expectedSubjectName, expectedIssuerName, expectedSerialNumber));
 };
 
