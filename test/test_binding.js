cbcf5c2c2f70b99c38ed24355a962f812541b9d0
Rajiv Shah <rajivshah1@icloud.com>
2021-04-15 14:03:51 -0400

    Set up CI

--- test/test_binding.js
+++ test/test_binding.js
@@ -1,12 +1,16 @@
 const VerifyAuthenticode = require("../dist/binding.js");
 const fs = require("fs");
 const assert = require("assert");
+const os = require("os");
 
 assert(VerifyAuthenticode.verify, "The expected function is undefined");
 
 function testBasic()
 {
-    const path = "C:\\Users\\rajiv\\Downloads\\firefly-desktop-0.3.1.exe";
+    let path = "C:\\Users\\rajiv\\Downloads\\firefly-desktop-0.3.1.exe";
+    if (process.env.CI) {
+        path = `${os.tmpdir()}\\test-file.exe`;
+    }
     assert(fs.statSync(path), "Path does not exist");
     const result = VerifyAuthenticode.verify(path, "IOTA Stiftung", "DigiCert SHA2 Assured ID Code Signing CA", "01 7e 53 37 de ca 4c 60 bd 18 81 e2 09 7e bd 3b");
     assert.strictEqual(result, true, "Unexpected value returned");
