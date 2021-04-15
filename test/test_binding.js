d42d9405a8abd2ee2e03c64b1215a30d0eb9c08f
Rajiv Shah <rajivshah1@icloud.com>
2021-04-15 14:23:03 -0400

    Fix tests

--- test/test_binding.js
+++ test/test_binding.js
@@ -5,17 +5,20 @@ const os = require("os");
 
 assert(VerifyAuthenticode.verify, "The expected function is undefined");
 
-function testBasic()
-{
+async function testBasic() {
     let path = "C:\\Users\\rajiv\\Downloads\\firefly-desktop-0.3.1.exe";
     if (process.env.CI) {
         path = `${os.tmpdir()}\\test-file.exe`;
     }
     assert(fs.statSync(path), "Path does not exist");
-    const result = VerifyAuthenticode.verify(path, "IOTA Stiftung", "DigiCert SHA2 Assured ID Code Signing CA", "01 7e 53 37 de ca 4c 60 bd 18 81 e2 09 7e bd 3b");
+    const result = await VerifyAuthenticode.verify(
+        path,
+        "IOTA Stiftung",
+        "DigiCert SHA2 Assured ID Code Signing CA",
+        "01 7e 53 37 de ca 4c 60 bd 18 81 e2 09 7e bd 3b"
+    );
     assert.strictEqual(result, true, "Unexpected value returned");
 }
 
-assert.doesNotThrow(testBasic, undefined, "testBasic threw an expection");
-
-console.log("Tests passed - everything looks OK!");
+assert.doesNotReject(testBasic, undefined, "testBasic threw an expection")
+    .then(() => console.log("Tests passed - everything looks OK!"));
